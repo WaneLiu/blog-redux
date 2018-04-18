02b5dc0b9a498006f96bbd98d0c632d4a4e24cd6
WaneLiu <825060952@qq.com>
2018-04-19 01:10:18 +0800

    redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+          //style="width: 900px; height: 400px;"
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
